fbf35a515060abcf0104f39254e8cb3282e97f37
GC-lap <2039799616@qq.com>
2021-10-26 20:29:59 +0800

    new

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
+
+	e.Logger.Fatal(e.Start(":8888"))
+}
+
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+}
+
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
