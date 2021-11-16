96f008aebaad1550c2a1ba9b2e9e331e9d5d4c46
GC-lap <2039799616@qq.com>
2021-11-16 12:26:30 +0800

    end to task3

---
+++ docs/struct.md
@@ -0,0 +1,11 @@
+### model
+model 层负责与数据库交互
+
+### controller
+controller 层是核心，具体逻辑实现层
+
+### router
+router 层是路由器层，负责 http 通信
+
+### server
+main
--- server.go
+++ server.go
@@ -1,12 +1,41 @@
 package main
 
 import (
-	"WebServer/router"
-	"github.com/labstack/echo/v4"
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+func mongoTest() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		fmt.Errorf("client establish failed. err: %v", err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err = client.Connect(ctx); err == nil {
+		fmt.Println("connect to db success.")
+	}
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	db := client.Database("Mongo")
+	collectionNames, err := db.ListCollectionNames(ctx, bson.M{})
+	fmt.Println("collectionNames:", collectionNames)
+}
+
 func main() {
-	e := echo.New()
-	router.RouterInit(e)
-	e.Logger.Fatal(e.Start(":2222"))
+	// e := echo.New()
+	// router.RouterInit(e)
+	// e.Logger.Fatal(e.Start(":2222"))
+	mongoTest()
 }
